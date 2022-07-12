d5f8f0d6723431cefe4f7b66af832948515145d5
Jahangir Jadi <jahangir.jadi@gmail.com>
2022-07-12 19:41:12 +0500

    Flutter image to text

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_image_to_text/utils/utils.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: ImageToText(),
+    );
+  }
+}
+
+class ImageToText extends StatefulWidget {
+  const ImageToText({Key? key}) : super(key: key);
+
+  @override
+  State<ImageToText> createState() => _ImageToTextState();
+}
+
+class _ImageToTextState extends State<ImageToText> {
+  File? _pickedImage;
+  String outputText = "";
+
+  pickedImage(File file) {
+    setState(() {
+      _pickedImage = file;
+    });
+
+    InputImage inputImage = InputImage.fromFile(file);
+    //code to recognize image
+    processImageForConversion(inputImage);
+  }
+
+  processImageForConversion(inputImage) async {
+    setState(() {
+      outputText = "";
+    });
+
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+
+    for (TextBlock block in recognizedText.blocks) {
+      setState(() {
+        outputText += block.text + "\n";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Image to Text")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          pickImage(ImageSource.gallery, pickedImage);
+        },
+        child: Icon(Icons.image),
+      ),
+      body: SizedBox(
+        height: double.infinity,
+        child: Column(children: [
+          if (_pickedImage == null)
+            Container(
+              height: 300,
+              color: Colors.black,
+              width: double.infinity,
+            )
+          else
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.file(
+                _pickedImage!,
+                fit: BoxFit.fill,
+              ),
+            ),
+          Expanded(child: Container()),
+          Text(
+            outputText,
+            style: TextStyle(fontSize: 24),
+          ),
+          Expanded(child: Container()),
+        ]),
+      ),
+    );
+  }
+}
